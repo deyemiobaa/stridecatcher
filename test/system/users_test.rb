77cfcf5afdb16e0d05c4e4cc6a350a462ad5ae37
sodiq <sodiqaderibigbe90@gmail.com>
2023-01-27 22:17:24 +0100

    try pipeline

---
+++ test/system/users_test.rb
@@ -0,0 +1,15 @@
+require "application_system_test_case"
+
+class UsersTest < ApplicationSystemTestCase
+  test "should register" do
+    visit new_user_registeration_path
+  
+    asser_difference("User.count") do
+      fill_in "Email", with: "unique@example.com"
+      fill_in "Password", with: "password"
+      fill_in "Password confirmation", with: "password"
+
+      click_button "Sign up"
+    end
+  end
+end
